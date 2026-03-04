1e1f45029f3b8ef845943f65755c4d4fb74b35ef
Fratch <57953411+Fratch@users.noreply.github.com>
2026-03-04 10:57:06 +0000

    Initial commit

---
+++ js/idroid.js
@@ -0,0 +1,7 @@
+document.querySelectorAll('.menu-item').forEach(item => {
+  item.addEventListener('click', () => {
+    document.querySelectorAll('.menu-item')
+      .forEach(i => i.classList.remove('active'));
+    item.classList.add('active');
+  });
+});
